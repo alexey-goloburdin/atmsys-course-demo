7d358e9ac5d988f377b9198ded60b07a0c244059
Alexey Goloburdin <sterx@rl6.ru>
2025-10-18 21:19:03 +0300

    lesson 101

---
+++ atmsys/exceptions.py
@@ -0,0 +1,20 @@
+class ATMException(Exception):
+    """Базовое исключение для банкомата"""
+    pass
+
+
+class PinCodeAttemptsExceed(ATMException):
+    """Закончились попытки ввода пин-кода"""
+    pass
+
+
+class InvalidAmount(ATMException):
+    """Некорректная сумма денег"""
+
+
+class InsufficientFunds(ATMException):
+    """Недостаточно средств для снятия со счёта"""
+
+
+class IncorrectMenuOption(ATMException):
+    """Выбран некорректный пункт меню"""
