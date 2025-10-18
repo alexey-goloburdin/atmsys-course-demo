af92c37553c969aebe3bd65c48152f73597f12c7
Alexey Goloburdin <sterx@rl6.ru>
2025-10-19 00:45:53 +0300

    lesson 104

---
+++ atmsys/ui.py
@@ -0,0 +1,13 @@
+class ConsoleUI:
+    """Пользовательский интерфейс банкомата"""
+    def show_message(self, message: str) -> None:
+        """Показывает сообщение message пользователю"""
+        print(message)
+    
+    def get_input(self, prompt: str) -> str:
+        """Запрашивает данные у пользователя и возвращает их"""
+        return input(prompt)
+    
+    def show_separator(self) -> None:
+        """Показывает визуальный разделитель"""
+        print(f"\n{'='*25}\n")
